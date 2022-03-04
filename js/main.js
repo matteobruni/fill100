30099831cdd997a25e90c0e5a1c4178f16ce75cd
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-03-04 14:35:11 +0100

    added comments to the javascript, it's easier to understand how it works

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (() => {
+  // game options
   const options = {
     width: 10,
     height: 10,
@@ -7,141 +8,25 @@
     },
   };
 
+  // tracking selected cell
   let currentPosition = 1;
 
+  // the dom container
   const gameTable = document.getElementById("game");
 
+  // possible moves, from the selected cell
   let possibleTargets = [];
 
-  const won = () => {
-    tsParticles.load("tsparticles", {
-      fullScreen: {
-        enable: true,
-        zIndex: 100,
-      },
-      particles: {
-        number: {
-          value: 0,
-        },
-        color: {
-          value: ["#00FFFC", "#FC00FF", "#fffc00"],
-        },
-        shape: {
-          type: ["circle", "square"],
-        },
-        opacity: {
-          value: 1,
-          animation: {
-            enable: true,
-            minimumValue: 0,
-            speed: 2,
-            startValue: "max",
-            destroy: "min",
-          },
-        },
-        size: {
-          value: 4,
-          random: {
-            enable: true,
-            minimumValue: 2,
-          },
-        },
-        links: {
-          enable: false,
-        },
-        life: {
-          duration: {
-            sync: true,
-            value: 5,
-          },
-          count: 1,
-        },
-        move: {
-          enable: true,
-          gravity: {
-            enable: true,
-            acceleration: 10,
-          },
-          speed: { min: 10, max: 20 },
-          decay: 0.1,
-          direction: "none",
-          straight: false,
-          outModes: {
-            default: "destroy",
-            top: "none",
-          },
-        },
-        rotate: {
-          value: {
-            min: 0,
-            max: 360,
-          },
-          direction: "random",
-          move: true,
-          animation: {
-            enable: true,
-            speed: 60,
-          },
-        },
-        tilt: {
-          direction: "random",
-          enable: true,
-          move: true,
-          value: {
-            min: 0,
-            max: 360,
-          },
-          animation: {
-            enable: true,
-            speed: 60,
-          },
-        },
-        roll: {
-          darken: {
-            enable: true,
-            value: 25,
-          },
-          enable: true,
-          speed: {
-            min: 15,
-            max: 25,
-          },
-        },
-        wobble: {
-          distance: 30,
-          enable: true,
-          move: true,
-          speed: {
-            min: -15,
-            max: 15,
-          },
-        },
-      },
-      interactivity: {
-        detectsOn: "window",
-        events: {
-          resize: true,
-        },
-      },
-      detectRetina: true,
-      emitters: {
-        direction: "none",
-        life: {
-          count: 0,
-          duration: 0.1,
-          delay: 0.4,
-        },
-        rate: {
-          delay: 0.1,
-          quantity: 150,
-        },
-        size: {
-          width: 0,
-          height: 0,
-        },
-      },
-    });
+  let confettiContainer;
 
+  // won function
+  const won = () => {
+    // tsParticles: https://github.com/matteobruni/tsparticles
+    tsParticles
+      .loadJSON("confetti", "particles/confetti.json")
+      .then((container) => (confettiContainer = container));
+
+    // bootstrap modal for winners
     var wonModal = new bootstrap.Modal(document.getElementById("wonModal"), {
       keyboard: false,
       focus: true,
@@ -151,67 +36,45 @@
     wonModal.show();
   };
 
+  // lost function, score will be displayed
   const lost = (score) => {
     if (confirm(`You reached: ${score}! Want to retry?`)) {
       drawTable();
     }
   };
 
+  // animated background initializer
   const initBgParticles = () => {
     // tsParticles: https://github.com/matteobruni/tsparticles
-    tsParticles.load("tsparticles", {
-      fullScreen: {
-        enable: true,
-        zIndex: -2,
-      },
-      particles: {
-        color: {
-          value: ["#222", "#333", "#072e07", "#300"],
-        },
-        number: {
-          value: 200,
-        },
-        move: {
-          enable: true,
-          speed: 2,
-        },
-        opacity: {
-          value: 1,
-        },
-        shape: {
-          type: ["square", "circle", "triangle"],
-        },
-        size: {
-          value: {
-            min: 50,
-            max: 100,
-          },
-          animation: {
-            enable: true,
-            speed: 5,
-            sync: false,
-          },
-        },
-      },
-    });
+    tsParticles.loadJSON("tsparticles", "particles/bg.json");
   };
 
+  // draw game table function, resets the game
   const drawTable = () => {
+    // resetting the target
     possibleTargets = [];
-    currentPosition = 1;
-    gameTable.innerHTML = "";
-    initBgParticles();
 
+    // resetting the current cell
+    currentPosition = 1;
+
+    // clearing previous game
+    gameTable.innerHTML = "";
+
+    // drawing rows
     for (let i = 0; i < options.width; i++) {
+      // creating the row element
       const tr = document.createElement("span");
 
       tr.classList.add("row");
 
+      // drawing cells
       for (let j = 0; j < options.height; j++) {
+        // creating the cell element
         const td = document.createElement("div");
 
         td.classList.add("cell");
 
+        // initializing the cell
         td.text = "";
         td.dataset.row = i;
         td.dataset.column = j;
@@ -224,109 +87,142 @@
     }
   };
 
+  // cell click handler
   const handleClick = (target) => {
-    if (currentPosition === 1 || possibleTargets.includes(target)) {
-      target.innerText = currentPosition++;
-
-      possibleTargets = [];
-
-      const row = parseInt(target.dataset.row);
-      const column = parseInt(target.dataset.column);
-      const indexes = [];
-
-      indexes.push({
-        row: row,
-        column: column + 3,
-      });
-      indexes.push({
-        row: row,
-        column: column - 3,
-      });
-
-      indexes.push({
-        row: row + 3,
-        column: column,
-      });
-      indexes.push({
-        row: row - 3,
-        column: column,
-      });
-
-      indexes.push({
-        row: row + 2,
-        column: column + 2,
-      });
-      indexes.push({
-        row: row + 2,
-        column: column - 2,
-      });
-
-      indexes.push({
-        row: row - 2,
-        column: column + 2,
-      });
-      indexes.push({
-        row: row - 2,
-        column: column - 2,
-      });
-
-      for (const tr of gameTable.childNodes) {
-        for (const td of tr.childNodes) {
-          td.classList.add("disabled");
-          td.classList.remove("active");
-        }
-      }
-
-      target.classList.add("active");
-
-      for (const index of indexes) {
-        const tr = gameTable.childNodes[index.row];
-
-        if (!tr) {
-          continue;
-        }
-
-        const td = tr.childNodes[index.column];
-
-        if (!td) {
-          continue;
-        }
-
-        if (!td.innerText) {
-          td.classList.remove("disabled");
-          possibleTargets.push(td);
-        }
-      }
-
-      setTimeout(() => {
-        if (!possibleTargets.length) {
-          if (currentPosition > options.width * options.height) {
-            won();
-          } else {
-            lost(currentPosition - 1);
-          }
-        } else if (possibleTargets.length === 1) {
-          setTimeout(() => {
-            handleClick(possibleTargets[0]);
-          }, 50);
-        }
-      }, 50);
+    // check if the click is valid
+    if (currentPosition !== 1 && !possibleTargets.includes(target)) {
+      return;
     }
+
+    // writing the current position in the selected cell
+    target.innerText = currentPosition++;
+
+    // resetting targets
+    possibleTargets = [];
+
+    // disabling all cells, the right ones will be reactivated
+    for (const tr of gameTable.childNodes) {
+      for (const td of tr.childNodes) {
+        td.classList.add("disabled");
+        td.classList.remove("active");
+      }
+    }
+
+    // setting the selected cell to active
+    target.classList.add("active");
+
+    // reading row and column of the selected cell
+    const row = parseInt(target.dataset.row), column = parseInt(target.dataset.column);
+
+    // iterating through possible indexes
+    const indexes = [];
+
+    // horizontal indexes
+    indexes.push({
+      row: row,
+      column: column + 3,
+    });
+
+    indexes.push({
+      row: row,
+      column: column - 3,
+    });
+
+    // vertical indexes
+    indexes.push({
+      row: row + 3,
+      column: column,
+    });
+
+    indexes.push({
+      row: row - 3,
+      column: column,
+    });
+
+    // diagonal indexes
+    indexes.push({
+      row: row + 2,
+      column: column + 2,
+    });
+
+    indexes.push({
+      row: row + 2,
+      column: column - 2,
+    });
+
+    indexes.push({
+      row: row - 2,
+      column: column + 2,
+    });
+
+    indexes.push({
+      row: row - 2,
+      column: column - 2,
+    });
+
+    // checking the possible indexes
+    for (const index of indexes) {
+      const tr = gameTable.childNodes[index.row];
+
+      // if the row is not existing, skipping
+      if (!tr) {
+        continue;
+      }
+
+      const td = tr.childNodes[index.column];
+
+      // if the cell is not existing, skipping
+      if (!td) {
+        continue;
+      }
+
+      // if the column has no text inside, is valid
+      if (!td.innerText) {
+        td.classList.remove("disabled");
+
+        possibleTargets.push(td);
+      }
+    }
+
+    // setting a small timeout, the UI thread needs to be freed
+    // using this, the user is going to understand what's happening
+    setTimeout(() => {
+      // checking if there are valid targets
+      if (!possibleTargets.length) {
+        // no more targets remaining, it's either a won or a loss
+        if (currentPosition > options.width * options.height) {
+          // it's a won, if the current position (1 indexed), is overflowing
+          won();
+        } else {
+          // if the current position is still a valid value, it's a loss
+          lost(currentPosition - 1);
+        }
+      } else if (possibleTargets.length === 1) {
+        // if there's only a possible target
+        // the move will be automatically done, using recursion
+        setTimeout(() => {
+          handleClick(possibleTargets[0]);
+        }, 50);
+      }
+    }, 50);
   };
 
-  // Object-GUI: https://github.com/matteobruni/object-gui
-
+  // initializing the game
   drawTable();
 
+  // initializing the animated background
+  initBgParticles();
+
+  // handling click event of the reset button
   document.getElementById("reset").addEventListener("click", () => {
     options.reset();
   });
 
+  // initializing the game editor, only for rows and columns
   let visibleEditor = false;
 
   document.getElementById("toggleEditor").addEventListener("click", () => {
-    console.log(visibleEditor);
-
+    // this is a toggle, if the editor is visible, it will become hidden
     if (visibleEditor) {
       visibleEditor = false;
 
@@ -338,6 +234,7 @@
     } else {
       visibleEditor = true;
 
+      // Object-GUI: https://github.com/matteobruni/object-gui
       const editor = new Editor("grid", "Grid Editor", options);
 
       editor.top().right().theme("neu-dark");
